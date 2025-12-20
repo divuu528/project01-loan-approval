b9eb80b7947e3585ef1bdbcba94ae958b4c2a389
divuu528 <divuu528@gmail.com>
2025-12-20 19:15:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 import os
-model_path=os.path.join("artifacts","kyc_logistic_model.pkl")
-model_path_scaler=os.path.join("artifacts","kyc_standard_scaler.pkl")
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model_path = os.path.join(BASE_DIR, "artifacts", "kyc_logistic_model.pkl")
+model_path_scaler = os.path.join(BASE_DIR, "artifacts", "kyc_standard_scaler.pkl")
+
